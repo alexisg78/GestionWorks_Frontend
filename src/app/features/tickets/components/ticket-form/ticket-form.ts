aa087a59064261224fd19cdcd5625cc5252a73dd
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-06-10 20:48:57 -0300

    feat: create ticket form component and integrate into ticket creation page

---
+++ src/app/features/tickets/components/ticket-form/ticket-form.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ticket-form',
+  imports: [],
+  templateUrl: './ticket-form.html',
+})
+export class TicketForm {}
--- src/app/features/tickets/pages/ticket-create-page/ticket-create-page.ts
+++ src/app/features/tickets/pages/ticket-create-page/ticket-create-page.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TicketForm } from '../../components/ticket-form/ticket-form';
 
 @Component({
   selector: 'app-ticket-create-page',
-  imports: [],
+  imports: [TicketForm],
   templateUrl: './ticket-create-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
