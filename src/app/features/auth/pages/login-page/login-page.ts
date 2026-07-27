e07abbf996b2ee23f781ed9d3edf76535093bf00
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-07-26 23:47:18 -0300

    feat: implement reactive login form

--- src/app/features/auth/pages/login-page/login-page.ts
+++ src/app/features/auth/pages/login-page/login-page.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
 })
-export default class LoginPage {}
+export default class LoginPage {
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  authService = inject(AuthService);
+
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+    this.authService.login(email, password).subscribe((resp) => {
+      console.log(resp);
+    });
+  }
+}
