bd5987f09f92a0ca4f1bf6b1f9b1879f65ff1ebd
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-08-01 11:08:33 -0300

    feat: add responsive public navbar with authentication support

--- src/app/layouts/public-layout/components/public-navbar/public-navbar.ts
+++ src/app/layouts/public-layout/components/public-navbar/public-navbar.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from '../../../../features/auth/services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'public-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './public-navbar.html',
 })
-export class PublicNavbar {}
+export class PublicNavbar {
+  authService = inject(AuthService);
+
+  initials = computed(() => {
+    const fullName = this.authService.user()?.fullName;
+
+    if (!fullName) return '';
+
+    const parts = fullName.trim().split(/\s+/);
+
+    return parts.length === 1
+      ? parts[0][0].toUpperCase()
+      : `${parts[0][0]}${parts[parts.length - 1][0]}`.toUpperCase();
+  });
+}
