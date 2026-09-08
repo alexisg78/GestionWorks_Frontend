d151d939de43f67e852f169e924d916f509a537a
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-09-07 21:35:16 -0300

    refactor(tickets): display dynamic labels for ticket status and priority

--- src/app/features/tickets/components/ticket-list/ticket-list.ts
+++ src/app/features/tickets/components/ticket-list/ticket-list.ts
@@ -1,11 +1,17 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TicketResponse } from '../../interfaces/ticket-response.interface';
+import { PRIORITY_LABELS } from '../../constants/priority-labels';
+import { STATUS_LABELS } from '../../constants/status-labels';
 
 @Component({
   selector: 'ticket-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ticket-list.html',
 })
 export class TicketList {
   tickets = input<TicketResponse[] | []>([]);
+
+  readonly priorityLabels = PRIORITY_LABELS;
+  readonly statusLabels = STATUS_LABELS;
 }
