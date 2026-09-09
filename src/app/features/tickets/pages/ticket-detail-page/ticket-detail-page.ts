8f83c8e9cf876c6c49742f7320d897a711112d94
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-09-08 23:39:33 -0300

    feat(tickets): add ticket detail page with loading and error states

--- src/app/features/tickets/pages/ticket-detail-page/ticket-detail-page.ts
+++ src/app/features/tickets/pages/ticket-detail-page/ticket-detail-page.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { TicketService } from '../../services/ticket.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'ticket-detail-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ticket-detail-page.html',
 })
-export default class TicketDetailPage {}
+export default class TicketDetailPage {
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly paramID = this.activatedRoute.snapshot.paramMap.get('id');
+
+  private readonly ticketService = inject(TicketService);
+
+  ticketResource = rxResource({
+    stream: () => this.ticketService.getTicket(this.paramID!),
+  });
+}
